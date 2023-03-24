d7d6b2587efc967186f0ea9381c8c4ec0ba7801e
Jimmy Mayta <jimmyymaytaj@gmail.com>
2023-03-23 21:08:05 -0400

    version 1

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const play = document.querySelector("#play");
+
+const audio = new Audio("http://stream.zeno.fm/1s53a2gkuf9uv");
+let audioplay = false;
+
+const playicon = () => {
+  if (audioplay) {
+    play.className = "bi-pause-fill text-white";
+  } else {
+    play.className = "bi-play-fill text-white";
+  }
+};
+
+const playaudio = async () => {
+  try {
+    if (audioplay) {
+      await audio.pause();
+      audioplay = !audioplay;
+    } else {
+      await audio.play();
+      audioplay = !audioplay;
+    }
+  } catch (error) {
+    console.log("Error al reproducir audio:", error);
+  }
+
+  playicon();
+};
+
+playicon();
+
+play.addEventListener("click", playaudio);
